737e71c3025f877a9ce0d65d7ca41c8524dcc1a1
oktavian aji <oktavian.aji@gmail.com>
2022-05-15 21:43:32 +0700

    create api for edit user profile

--- account-service/src/api/controllers/index.js
+++ account-service/src/api/controllers/index.js
@@ -5,4 +5,5 @@ module.exports = {
   logoutController: require('./logout.controller'),
   changePasswordController: require('./changePassword.controller'),
   profileController: require('./profile.controller'),
+  editProfileController: require('./editProfile.controller'),
 }
