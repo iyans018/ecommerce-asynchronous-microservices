46375c15d532d7496e556f3323917a143ddc40c7
oktavian aji <oktavian.aji@gmail.com>
2022-05-16 10:14:10 +0700

    create initial routes

--- product-service/src/config/app.js
+++ product-service/src/config/app.js
@@ -4,11 +4,15 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const { productRoutes } = require('../api/routes');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+
+app.use(productRoutes);
 
 module.exports = app;
